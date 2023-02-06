988ede9749b37f3a94ba72429f2bc678945665ce
Mehdi <96rajabi@gmail.com>
2023-02-06 21:11:14 +0330

    Added Verification

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -4,11 +4,15 @@ namespace Rajabit\Discord\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Rajabit\Discord\InteractionResponseType;
+use Rajabit\Discord\InteractionType;
 
 class WebhookController
 {
+
     public function index(Request $request)
     {
+
         $array = [
             "schemeAndHttpHost" => $request->schemeAndHttpHost(),
             "method" => $request->method(),
@@ -16,10 +20,14 @@ class WebhookController
             "headers" => $request->header()
         ];
 
-
         Storage::disk('local')
             ->put('discord.log', json_encode($array, JSON_PRETTY_PRINT) . "\n\n");
 
-        return response()->json($array);
+        return response()->json([
+            "type" => InteractionResponseType::CHANNEL_MESSAGE_WITH_SOURCE,
+            "data" => [
+                "content" => 'Hello world',
+            ]
+        ]);
     }
 }
--- src/Http/Middleware/WebhookAuthenticateMiddleware.php
+++ src/Http/Middleware/WebhookAuthenticateMiddleware.php
@@ -10,7 +10,34 @@ class WebhookAuthenticateMiddleware
 
     public function handle(Request $request, Closure $next)
     {
+        /**
+         * 
+         * Validate discord interaction request
+         * 
+         */
 
+        $signature = $request->header('X-Signature-Ed25519');
+        $timestamp = $request->header('X-Signature-Timestamp');
+
+        if (empty($signature) || empty($timestamp))
+            return abort(401);
+
+
+        if (!trim($signature, '0..9A..Fa..f') == '') {
+            return abort(401);
+        }
+
+        $message = $timestamp . $request->getContent();
+        $binary_signature = sodium_hex2bin($signature);
+        $binary_key = sodium_hex2bin(config('discord.public'));
+
+        if (!sodium_crypto_sign_verify_detached(
+            $binary_signature,
+            $message,
+            $binary_key
+        )) {
+            return abort(401);
+        }
 
         return $next($request);
     }
